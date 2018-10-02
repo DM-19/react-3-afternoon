1a270978a474dd84dfb32e30dc8f9b52adaf0e39
Daniel De Hoyos <dsdehoyos@gmail.com>
2018-10-02 16:37:45 -0500

    done2.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from '../components/Post/Post';
 
 class App extends Component {
   constructor() {
@@ -13,29 +15,56 @@ class App extends Component {
       posts: []
     };
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
+    this.updatePost = this.updatePost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.createPost = this.createPost.bind(this);
   }
-  
+
   componentDidMount() {
+    axios.get('https://practiceapi.devmountain.com/api/posts').then(results => {
+      this.setState({ posts: results.data })
+    })
 
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, { text }).then(results => {
+      this.setState({ posts: results.data })
+    })
+
   }
 
-  deletePost() {
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`).then(results => {
+      this.setState({ posts: results.data })
+    })
 
   }
 
-  createPost() {
+  createPost(text) {
+    axios.post('https://practiceapi.devmountain.com/api/posts', { text }).then(results => {
+      this.setState({ posts: results.data })
+    })
 
   }
 
   render() {
     const { posts } = this.state;
+    console.log("RENDER", posts)
+
+    let postsRend = posts.map(post => (
+      <Post
+        date={post.date}
+        text={post.text}
+        key={post.id}
+        id={post.id}
+        updatePostFn={this.updatePost}
+        deletePostFn={this.deletePost}
+      />
+    ))
+
+    console.log(posts)
+
 
     return (
       <div className="App__parent">
@@ -43,8 +72,12 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
-          
+          <Compose
+            createPostFn={this.createPost}
+          />
+
+          {postsRend}
+
         </section>
       </div>
     );
